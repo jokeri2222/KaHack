e59368ae9d687ddb66aa9b91745212d107f408b8
jokeri2222 <94902709+jokeri2222@users.noreply.github.com>
2024-01-10 09:53:59 +0200

    Create KaHack.meta.js

---
+++ KaHack.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         KaHack!
+// @version      1.0.15
+// @namespace    https://github.com/jokeri2222
+// @description  A hack for kahoot.it!
+// @updateURL    https://github.com/jokeri2222/KaHack/raw/main/KaHack!.meta.js
+// @downloadURL  https://github.com/jokeri2222/KaHack/raw/main/KaHack!.user.js
+// @author       jokeri2222; https://github.com/jokeri2222
+// @match        https://kahoot.it/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kahoot.it
+// @grant        none
+// ==/UserScript==
