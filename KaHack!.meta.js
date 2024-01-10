3e9846179790e7b0d294f04fff75365c4f9332b6
jokeri2222 <94902709+jokeri2222@users.noreply.github.com>
2024-01-10 10:43:20 +0200

    Update KaHack!.meta.js

--- KaHack!.meta.js
+++ KaHack!.meta.js
@@ -3,8 +3,8 @@
 // @version      1.0.15
 // @namespace    https://github.com/jokeri2222
 // @description  A hack for kahoot.it!
-// @updateURL    https://github.com/jokeri2222/KaHack-/raw/main/KaHack!.meta.js
-// @downloadURL  https://github.com/jokeri2222/KaHack-/raw/main/KaHack!.user.js
+// @updateURL    https://github.com/jokeri2222/Kahoot-Hack/raw/main/KaHack!.meta.js
+// @downloadURL  https://github.com/jokeri2222/Kahoot-Hack/raw/main/KaHack!.user.js
 // @author       jokeri2222; https://github.com/jokeri2222
 // @match        https://kahoot.it/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kahoot.it
