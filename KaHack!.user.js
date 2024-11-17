bda1ae223466764f6f73b31a919e5d0c0cda83c5
jokeri2222 <94902709+jokeri2222@users.noreply.github.com>
2024-11-17 17:21:48 +0200

    Update KaHack!.user.js

--- KaHack!.user.js
+++ KaHack!.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
 // @name         KaHack!
-// @version      1.0.24
+// @version      1.0.25
 // @namespace    https://github.com/jokeri2222
 // @description  A hack for kahoot.it!
-// @updateURL    https://github.com/jokeri2222/Kahoot-Hack/raw/main/KaHack!.meta.js
-// @downloadURL  https://github.com/jokeri2222/Kahoot-Hack/raw/main/KaHack!.user.js
+// @updateURL    https://github.com/jokeri2222/KaHack/raw/main/KaHack!.meta.js
+// @downloadURL  https://github.com/jokeri2222/KaHack/raw/main/KaHack!.user.js
 // @author       jokeri2222; https://github.com/jokeri2222
 // @match        https://kahoot.it/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kahoot.it
 // @grant        none
 // ==/UserScript==
-
-var Version = '1.0.24'
+var Version = '1.0.25'
 
 var questions = [];
 var info = {
